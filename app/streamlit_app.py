75c7504424c5674ee50d63f5377d7a28f09a514b
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-22 21:22:41 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -172,7 +172,7 @@ def initial_assessment():
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            margin-bottom: 20px;
+            margin-bottom: 0px;
             color: #241000;
             min-height: 80px;
         }
