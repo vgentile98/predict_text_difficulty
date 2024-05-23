09623d907e219f05e5f2f363f22ebf31acf60a94
vgentile98 <147622625+vgentile98@users.noreply.github.com>
2024-05-23 12:40:50 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -306,9 +306,7 @@ def initial_assessment():
         st.subheader("Let\'s start our adventure with a game")
         st.write("Read each sentence and choose the main idea from the options below. Let\'s see how much you already know!")
     with col2:
-        st.image("https://raw.githubusercontent.com/vgentile98/predict_text_difficulty/main/app/images/baguette_progress.png", width=200)
-    st.image("https://raw.githubusercontent.com/vgentile98/predict_text_difficulty/main/app/images/baguette_salut.png", width=200)
-
+        st.image("https://raw.githubusercontent.com/vgentile98/predict_text_difficulty/main/app/images/baguette_salut.png", width=200)
         
     questions = [
         ("Le restaurant 'Bon appétit' recherche des serveurs pour l'été.", ["A restaurant is closing for the summer.", "A restaurant is looking for summer staff.", "A restaurant is changing its menu."], 1, 'A1'),
